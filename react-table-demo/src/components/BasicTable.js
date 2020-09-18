3d752e3250bff5a99d47d4703c481de3bbebe339
Vishwas <vishwas.g5@gmail.com>
2020-09-18 20:03:12 +0530

    Add basic table

---
+++ react-table-demo/src/components/BasicTable.js
@@ -0,0 +1,47 @@
+import React, { useMemo } from 'react'
+import { useTable } from 'react-table'
+import MOCK_DATA from './MOCK_DATA.json'
+import { COLUMNS } from './columns'
+import './table.css'
+
+export const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, [])
+  const data = useMemo(() => MOCK_DATA, [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  })
+
+  return (
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
---
+++ react-table-demo/src/components/columns.js
@@ -0,0 +1,26 @@
+export const COLUMNS = [
+  {
+    Header: 'Id',
+    accessor: 'id'
+  },
+  {
+    Header: 'First Name',
+    accessor: 'first_name'
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'last_name'
+  },
+  {
+    Header: 'Date of Birth',
+    accessor: 'date_of_birth'
+  },
+  {
+    Header: 'Country',
+    accessor: 'country'
+  },
+  {
+    Header: 'Phone',
+    accessor: 'phone'
+  }
+]
