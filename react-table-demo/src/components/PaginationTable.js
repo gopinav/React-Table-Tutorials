6af7b57dd998c0c2cc9bcafedd539e6aab0dea0a
Vishwas <vishwas.g5@gmail.com>
2020-09-26 13:45:01 +0530

    Pagination - gotoPage

--- react-table-demo/src/components/PaginationTable.js
+++ react-table-demo/src/components/PaginationTable.js
@@ -17,14 +17,21 @@ export const PaginationTable = () => {
     previousPage,
     canPreviousPage,
     canNextPage,
+    pageOptions,
+    state,
+    gotoPage,
+    pageCount,
     prepareRow
   } = useTable(
     {
       columns,
-      data
+      data,
+      initialState: { pageIndex: 2 }
     },
     usePagination
   )
+
+  const { pageIndex } = state
 
   return (
     <>
@@ -52,12 +59,36 @@ export const PaginationTable = () => {
         </tbody>
       </table>
       <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {'<<'}
+        </button>{' '}
         <button onClick={() => previousPage()} disabled={!canPreviousPage}>
           Previous
         </button>{' '}
         <button onClick={() => nextPage()} disabled={!canNextPage}>
           Next
         </button>{' '}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {'>>'}
+        </button>{' '}
+        <span>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{' '}
+        </span>
+        <span>
+          | Go to page:{' '}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={e => {
+              const pageNumber = e.target.value ? Number(e.target.value) - 1 : 0
+              gotoPage(pageNumber)
+            }}
+            style={{ width: '50px' }}
+          />
+        </span>{' '}
       </div>
     </>
   )
