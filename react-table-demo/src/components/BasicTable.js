e1173820fa708f7a4fd713ce84126256534e943c
Vishwas <vishwas.g5@gmail.com>
2020-09-26 18:18:07 +0530

    Basic table component

--- react-table-demo/src/components/BasicTable.js
+++ react-table-demo/src/components/BasicTable.js
@@ -1,21 +1,12 @@
 import React, { useMemo } from 'react'
-import { useTable, useSortBy, useFilters, useGlobalFilter } from 'react-table'
+import { useTable } from 'react-table'
 import MOCK_DATA from './MOCK_DATA.json'
 import { COLUMNS } from './columns'
 import './table.css'
-import { GlobalFilter } from './GlobalFilter'
-import { ColumnFilter } from './ColumnFilter'
 
 export const BasicTable = () => {
   const columns = useMemo(() => COLUMNS, [])
   const data = useMemo(() => MOCK_DATA, [])
-
-  const defaultColumn = React.useMemo(
-    () => ({
-      Filter: ColumnFilter
-    }),
-    []
-  )
 
   const {
     getTableProps,
@@ -23,41 +14,20 @@ export const BasicTable = () => {
     headerGroups,
     footerGroups,
     rows,
-    prepareRow,
-    state,
-    setGlobalFilter
-  } = useTable(
-    {
-      columns,
-      data,
-      defaultColumn
-    },
-    useFilters,
-    useGlobalFilter,
-    useSortBy
-  )
-
-  const { globalFilter } = state
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  })
 
   return (
     <>
-      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
       <table {...getTableProps()}>
         <thead>
           {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map(column => (
-                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                  {column.render('Header')}
-                  <span>
-                    {column.isSorted
-                      ? column.isSortedDesc
-                        ? ' 🔽'
-                        : ' 🔼'
-                      : ''}
-                  </span>
-                  <div>{column.canFilter ? column.render('Filter') : null}</div>
-                </th>
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
               ))}
             </tr>
           ))}
