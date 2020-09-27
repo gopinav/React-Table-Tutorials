afac3932fe2992671e5e6671a95211000c3f87a0
Vishwas <vishwas.g5@gmail.com>
2020-09-27 10:53:27 +0530

    Row selection

--- react-table-demo/src/App.js
+++ react-table-demo/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './App.css'
-import { PaginationTable } from './components/PaginationTable'
+import { RowSelection } from './components/RowSelection'
 
 function App() {
   return (
     <div className='App'>
-      <PaginationTable />
+      <RowSelection />
     </div>
   )
 }
---
+++ react-table-demo/src/components/RowSelection.js
@@ -0,0 +1,79 @@
+import React, { useMemo } from 'react'
+import { useTable, useRowSelect } from 'react-table'
+import MOCK_DATA from './MOCK_DATA.json'
+import { COLUMNS } from './columns'
+import './table.css'
+import { Checkbox } from './Checkbox'
+
+export const RowSelection = () => {
+  const columns = useMemo(() => COLUMNS, [])
+  const data = useMemo(() => MOCK_DATA, [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    selectedFlatRows
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useRowSelect,
+    hooks => {
+      hooks.visibleColumns.push(columns => [
+        {
+          id: 'selection',
+          Header: ({ getToggleAllRowsSelectedProps }) => (
+            <Checkbox {...getToggleAllRowsSelectedProps()} />
+          ),
+          Cell: ({ row }) => <Checkbox {...row.getToggleRowSelectedProps()} />
+        },
+        ...columns
+      ])
+    }
+  )
+
+  const firstPageRows = rows.slice(0, 10)
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {firstPageRows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedFlatRows: selectedFlatRows.map(row => row.original)
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
+    </>
+  )
+}
