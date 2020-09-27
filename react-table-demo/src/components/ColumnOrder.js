89a3164533a4794ae12af67fe6ccf0992d9be764
Vishwas <vishwas.g5@gmail.com>
2020-09-27 13:13:50 +0530

    ColumnOrdering feature

---
+++ react-table-demo/src/components/ColumnOrder.js
@@ -0,0 +1,65 @@
+import React, { useMemo } from 'react'
+import { useTable, useColumnOrder } from 'react-table'
+import MOCK_DATA from './MOCK_DATA.json'
+import { COLUMNS } from './columns'
+import './table.css'
+
+export const ColumnOrder = () => {
+  const columns = useMemo(() => COLUMNS, [])
+  const data = useMemo(() => MOCK_DATA, [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+    setColumnOrder
+  } = useTable({
+    columns,
+    data
+  }, useColumnOrder)
+
+  const changeOrder = () => {
+      setColumnOrder(['id', 'first_name', 'last_name', 'phone', 'country', 'date_of_birth'])
+  }
+
+  return (
+    <>
+    <button onClick={changeOrder}>Change column order</button>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map(footerGroup => (
+            <tr {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map(column => (
+                <td {...column.getFooterProps()}>{column.render('Footer')}</td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </>
+  )
+}
